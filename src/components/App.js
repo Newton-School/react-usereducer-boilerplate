fa6dc2a78e5502355172ccd3ff5734dccb799fa0
shivangbhardwaj2001 <shivangbhardwaj2001@gmail.com>
2022-12-01 19:03:15 +0530

    pushing

---
+++ src/components/App.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import '../styles/App.css';
+import { useReducer } from 'react';
+
+const ACTIONS = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+  SET_SUB_NUM: "setSubNum",
+  SET_ADD_NUM: "setAddNum"
+};
+function reducer(state, action) {
+//code here 
+
+
+}
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    count: 10,
+    subNum: 1,
+    addNum: 1
+  });
+
+  function onIncrement() {
+    dispatch({ type: ACTIONS.INCREMENT });
+  }
+  function onDecrement() {
+    dispatch({ type: ACTIONS.DECREMENT });
+  }
+  const onAddInput = (e) => {
+    dispatch({
+      type: ACTIONS.SET_ADD_NUM,
+      payload: e.target.value
+    });
+  };
+  const onSubtractInput = (e) => {
+    dispatch({
+      type: ACTIONS.SET_SUB_NUM,
+      payload: e.target.value
+    });
+  };
+  return (
+    <div id="main">
+      <input id='subtractInput' value={state.subNum} onChange={onSubtractInput} /><br />
+      <button id='subtractBtn' onClick={onDecrement}>Subtract</button>
+      <p id='count'>{state.count}</p>
+      <button id='addBtn' onClick={onIncrement}>Add</button><br />
+      <input id='addInput' onChange={onAddInput} value={state.addNum} />
+    </div>
+  )
+}
+
+
+export default App;
